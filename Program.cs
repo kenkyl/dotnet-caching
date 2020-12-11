c5d03e9b55de6fb5d0aa11b64a5416bad40ca200
Kyle Kennedy <kylekennedy@Kyles-MacBook-Pro.local>
2020-12-11 10:11:37 -0500

    fetch repl working

--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ namespace dotnet_caching
         public string name { get; set; }
         public String email { get; set; }
         public String country { get; set; }
+
+        public override string ToString()
+        {
+            return id + "--" + name + "--" + country + "--" + email;
+        }
     }
 
     class Program
@@ -27,6 +32,7 @@ namespace dotnet_caching
 
 
         static void LoadData(MySqlConnection conn) {
+            // Create Users table if it does not exist 
             try 
             {
                 MySqlCommand createTable = new MySqlCommand(@"
@@ -42,13 +48,21 @@ namespace dotnet_caching
             }
             catch (Exception ex)
             {
-
+                Console.WriteLine($"Error: {ex.ToString()}");
             }
 
-            // load database with dummy user info
+            // load database with dummy user info for 10 users\
             List<User> users = new List<User>() {
                 new User() {id=1, name="Fred Angerman", country="US", email="angerfred@mail.com"},
-                new User() {id=2, name="Carrie Bartine", country="CA", email="bartcar@achoo.com"}
+                new User() {id=2, name="Carrie Bartine", country="CA", email="bartcar@achoo.com"},
+                new User() {id=3, name="Gorth Borgenson", country="DE", email="gorth@mail.com"},
+                new User() {id=4, name="Rose Ramirez", country="US", email="rosera@achoo.com"},
+                new User() {id=5, name="Barry Williams", country="US", email="willbarry@mail.com"},
+                new User() {id=6, name="Sue Parker", country="SE", email="swedesue@achoo.com"},
+                new User() {id=7, name="Jonathan Smolinksi", country="DE", email="smoljon@mail.com"},
+                new User() {id=8, name="Gertrude Fine", country="FR", email="gert.fine@achoo.com"},
+                new User() {id=9, name="Anthony Leotardo", country="IT", email="leoan@mail.com"},
+                new User() {id=10, name="Cindy Smithe", country="US", email="smindy@achoo.com"}
             };
             
             foreach (var user in users) {
@@ -61,7 +75,10 @@ namespace dotnet_caching
             }
         }
 
-        static void FetchUser(IDatabase dbRedis, MySqlConnection conn, int userId) {
+        static (string, int) FetchUser(IDatabase dbRedis, MySqlConnection conn, int userId) {
+            int startTime = 0; 
+            int endTime = 0;
+            string fetchedUser = "";
             // attempt to fetch from cache 
             // if failed, fetch from db 
             // if fetched from db, load cache 
@@ -73,25 +90,21 @@ namespace dotnet_caching
 
             while (rdr.Read())
             {
-                Console.WriteLine(rdr[0]+" -- "+rdr[1]+" -- "+rdr[2]+" -- "+rdr[3]);
+                fetchedUser = rdr[0]+" -- "+rdr[1]+" -- "+rdr[2]+" -- "+rdr[3];
+                Console.WriteLine(fetchedUser);
             }
             rdr.Close();
+            int queryTime = endTime - startTime; 
+
+            return (fetchedUser, queryTime);
         }
 
         static void Main(string[] args)
         {
-            // establsih mysql connection 
-
-
             // establish redis connection
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect($"{redisHost}:{redisPort}");
             IDatabase dbRedis = redis.GetDatabase();
-            string value = "abcdefg";
-            dbRedis.StringSet("mykey", value);
             
-            string value2 = dbRedis.StringGet("mykey");
-            Console.WriteLine(value2);
-
             // establish mysql connection
             string connStr = $"server={mysqlHost};user={mysqlUser};database={mysqlDatabase};port=3306;password=password";
             MySqlConnection conn = new MySqlConnection(connStr);
@@ -99,11 +112,10 @@ namespace dotnet_caching
             {
                 Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
-                // Perform database operations
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine($"Error: {ex.ToString()}");
             }
 
 
@@ -111,11 +123,26 @@ namespace dotnet_caching
             FetchUser(dbRedis, conn, 1);
             
 
+            
+
+            while (true) {
+                Console.WriteLine("\nEnter a command: ");
+                var command = Console.ReadLine();
+                string[] words = command.Split(' ');
+                if (words[0].Equals("quit")) 
+                {
+                    break;
+                }
+                else if (words[0].Equals("get")) {
+                    var id = Int32.Parse(words[1]); 
+                    (string user, int time) res = FetchUser(dbRedis, conn, id);
+                    Console.WriteLine($"\nExecution time: {res.time}\nFetched user: {res.user}");
+                }
+
+            }
+
             conn.Close();
             Console.WriteLine("Done.");
-
-
-
 
             // Console.WriteLine("\nWhat is your name? ");
             // var name = Console.ReadLine();
